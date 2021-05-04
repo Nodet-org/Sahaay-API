0b200a1239b599aea9d18fa6899a8589b3e261e3
Nandan-unni <asnqln@gmail.com>
2021-05-04 22:34:44 +0530

    fetch tweets

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const twitter  = require("static-tweets");
 
 var app = express();
 
@@ -8,6 +9,18 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Everything started with a blast.");
+});
+
+app.get("/api/tweets", async (req, res) => {
+  const tweetsId = req.body.tweets;
+  const tweetsJSON = [];
+  for (let i = 0; i < tweetsId.length; i++) {
+    const tweetJSON = await twitter.fetchTweetAst(tweetsId[i]);
+    tweetsJSON.push(tweetJSON);
+  }
+  return res.status(200).json({
+    tweets: tweetsJSON,
+  });
 });
 
 app.use("/public/static", express.static(path.join(__dirname, "static")));
